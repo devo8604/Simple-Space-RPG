79823012c225a09c92135d6066fc28d1a9333775
G89390 <G89390@lak1pce00240793.northgrum.com>
2013-02-18 10:01:21 -0900

    fully implemented XML reader for NPCs.xml, with for loop and an arraylist to show all enemy encounters, which can be accessed globally.

--- SimpleSpace/src/SimpleSpace/initGameData.java
+++ SimpleSpace/src/SimpleSpace/initGameData.java
@@ -25,6 +25,7 @@ public class initGameData {
     initGameData() {
         try {
             setPrologue();
+            setNPCs();
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(initGameData.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SAXException ex) {
@@ -44,7 +45,51 @@ public class initGameData {
         return prologueTXT;
     }
     
-    public void setNPCs() {
+    public void setNPCs() throws ParserConfigurationException, SAXException, IOException {
+        Document NPCDATA = fileData.readXMLinJAR("data/NPCs.xml");
+        NodeList nl = NPCDATA.getDocumentElement().getElementsByTagName("NPC");
+        for(int ctr = 0;ctr < nl.getLength(); ctr++) {
+            npcs.add(new entity());
+            for(int i = 0; i < nl.item(ctr).getChildNodes().getLength(); i++) {
+                if(nl.item(ctr).getChildNodes().item(i).getLocalName() != null) {
+                    if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("isAlive")) {
+                        npcs.get(ctr).isAlive = true;
+                        System.out.println("Found isAlive value! Setting NPC's isAlive value to: " +
+                                npcs.get(ctr).isAlive);
+                    }
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("HP")) {
+                        npcs.get(ctr).HP = Double.parseDouble(nl.item(ctr).getChildNodes().item(i).getTextContent());
+                        System.out.println("Found HP value! Setting NPC's HP value to: " +
+                                npcs.get(ctr).HP);
+                    }
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("dmgModifier")) {
+                        npcs.get(ctr).dmgModifier = Double.parseDouble(nl.item(ctr).getChildNodes().item(i).getTextContent());
+                        System.out.println("Found dmgModifier value! Setting NPC's dmgModifier value to: " +
+                                npcs.get(ctr).dmgModifier);
+                    }
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("lootvalue")) {
+                        npcs.get(ctr).lootvalue = Integer.parseInt(nl.item(ctr).getChildNodes().item(i).getTextContent());
+                        System.out.println("Found loot value! Setting NPC's loot value to: " +
+                                npcs.get(ctr).lootvalue); 
+                    }   
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("lootrolls")) {
+                        npcs.get(ctr).lootrolls = Integer.parseInt(nl.item(ctr).getChildNodes().item(i).getTextContent());
+                        System.out.println("Found loot roll value! Setting NPC's loot roll value to: " +
+                                npcs.get(ctr).lootrolls);
+                    }
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("name")) {
+                        npcs.get(ctr).name = nl.item(ctr).getChildNodes().item(i).getTextContent();
+                        System.out.println("Found name value! Setting NPC's name value to: " +
+                                npcs.get(ctr).name); 
+                    }
+                    else if(nl.item(ctr).getChildNodes().item(i).getLocalName().equals("initSpam")) {
+                        npcs.get(ctr).initSpam = nl.item(ctr).getChildNodes().item(i).getTextContent();
+                        System.out.println("Found initSpam value! Setting NPC's initSpam value to: " +
+                                npcs.get(ctr).initSpam);  
+                    }                    
+                }
+            }
+        }
 
     }
 }
