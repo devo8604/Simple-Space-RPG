5c4035d3c8b8c3d5e669d3d5a8559a2f674626cb
g96930 <g96930@LAK1PCE00227488.northgrum.com>
2013-02-19 15:00:34 -0900

    Added inventory menu

--- SimpleSpace/src/SimpleSpace/inventory.java
+++ SimpleSpace/src/SimpleSpace/inventory.java
@@ -1,20 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package SimpleSpace;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  *
- * @author g96930
+ * @author Devon Smith
  */
 public class inventory {
-
+    
+ 
     public ArrayList<item> possibleItems;
     String describeItem;
+
+    public void inventoryMenu(Scanner in) throws ParserConfigurationException, SAXException, IOException, InterruptedException {
+        simpleSpace simple = new simpleSpace();
+        
+        System.out.println("|--------------------|");
+        System.out.println("|  Select one:       |");
+        System.out.println("|  1. Jump           |");
+        System.out.println("|  2. **reserved**   |");
+        System.out.println("|  3. **reserved**   |");
+        System.out.println("|  4. Main Menu      |");
+        System.out.println("|--------------------|");
+        
+        int menuItem = in.nextInt();
+        switch(menuItem) {
+            case 1:
+                listInv();  //lists player inventory
+              break;
+            case 2:
+                //future use
+              break;
+            case 3:
+                //future use
+            case 4:
+                simple.startGameEvent(in);
+        }
+        
+    }
 
     public String listInv() {
         for (int i = 0; i <= possibleItems.size(); i++) {
