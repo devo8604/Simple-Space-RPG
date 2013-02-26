c8f88233d6b4f29067cf95f3051b3b98c02d6719
g96930 <g96930@LAK1PCE00227488.northgrum.com>
2013-02-26 11:38:54 -0900

    Added repair method.

--- SimpleSpace/src/SimpleSpace/GameData.java
+++ SimpleSpace/src/SimpleSpace/GameData.java
@@ -23,7 +23,7 @@ public class GameData extends initGameData {
         System.out.println("|--------------------|");
         System.out.println("|  Select one:       |");
         System.out.println("|  1. List inventory |");
-        System.out.println("|  2. **reserved**   |");
+        System.out.println("|  2. Repair         |");
         System.out.println("|  3. **reserved**   |");
         System.out.println("|  4. Main Menu      |");
         System.out.println("|--------------------|");
@@ -36,7 +36,7 @@ public class GameData extends initGameData {
                     System.out.println(listInv());  //lists player inventory
                     break;
                 case 2:
-                    //future use
+                    repair();
                     break;
                 case 3:
                     //future use
@@ -61,5 +61,20 @@ public class GameData extends initGameData {
                     + " Damage Val " + possibleItems.get(i).dmgValue + "\n";
         }
         return describeItem;
-    }    
+    } 
+    public void repair() {
+        Scanner in = new Scanner(System.in);
+        initGameData data = new initGameData();
+        System.out.println("\nYour current HP is at " + data.plyrs.get(0).HP);
+        System.out.println("You have " + data.possibleItems.get(2).qty + " Repair Drones.\n" 
+                + "and each have a repair value of " + data.possibleItems.get(2).repairValue + " HP."
+                + "\nHow many do you want to use?");
+        int menuItem = in.nextInt();
+        if (data.possibleItems.get(2).qty >= menuItem) {
+            data.plyrs.get(0).HP += (data.possibleItems.get(2).repairValue * menuItem);
+            System.out.println("You have " + data.plyrs.get(0).HP + " HP");
+        } else {
+            System.out.println("You do not have enough drones!");
+        }
+    }
 }
